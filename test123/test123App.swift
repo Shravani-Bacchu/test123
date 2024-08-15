220a22f089666544d859c646b4b7549c383e8b2b
Shravani <shravanibacchu@gmail.com>
2024-08-15 13:28:45 +0100

    Initial Commit

---
+++ test123/test123App.swift
@@ -0,0 +1,17 @@
+//
+//  test123App.swift
+//  test123
+//
+//  Created by Scholar on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct test123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
